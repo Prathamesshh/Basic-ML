6a33e2c928e852b901bf1c946d843da8a55e8c2c
Prathamesshh <prathameshkurekar@gmail.com>
2025-05-18 22:44:08 +0530

    add

---
+++ flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+'''
+This is a simple Flask application that serves a static HTML page.
+'''
+
+### WSGI Application
+### This is the entry point for the Flask application.
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to flask course"
+
+if __name__ == '__main__':
+    app.run()
+
